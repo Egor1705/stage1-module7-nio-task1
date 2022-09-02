abead21038b25d14db6db2fb5764f7249ea96a85
Egor1705 <BsuHelp@tut.by>
2022-09-02 20:36:50 +0300

    java.nio

--- src/main/java/com/epam/mjc/nio/FileReader.java
+++ src/main/java/com/epam/mjc/nio/FileReader.java
@@ -1,11 +1,36 @@
 package com.epam.mjc.nio;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 
 public class FileReader {
 
-    public Profile getDataFromFile(File file) {
-        return new Profile();
+    public Profile getDataFromFile(File file)  {
+
+        String[] parts = null;
+        try{String content = new String(Files.readAllBytes(Paths.get(file.getPath())));
+
+
+            parts = content.split("\n");
+            System.out.println(content);
+        }
+        catch(IOException f){
+            f.printStackTrace();
+        }
+
+
+
+        String[] parts0 = parts[0].split(": ");
+        String[] parts1 = parts[1].split(": ");
+        String[] parts2 = parts[2].split(": ");
+        String[] parts3 = parts[3].split(": ");
+
+
+
+        return new Profile(parts0[1].replaceAll("\\s",""), Integer.parseInt(parts1[1].replaceAll("\\s","")),
+                parts2[1].replaceAll("\\s",""), Long.parseLong(parts3[1].replaceAll("\\s","")));
     }
 }
